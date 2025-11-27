10cc3888532191161ae9cde441727c745b35a764
Gift Ndoma <giftndoma87@gmail.com>
2025-11-27 07:55:18 +0100

    added swagger.js file

---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: 'E-commerce API',
+        description: 'API documentation for the E-commerce application',
+    },
+    host: 'localhost:4000',
+    schemes: ['http'],
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./server.js'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
